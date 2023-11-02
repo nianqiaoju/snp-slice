6bcbeb4e7e2ec2d74c693d0f40b284cee73bd07f
Nianqiao <12590785+nianqiaoju@users.noreply.github.com>
2023-11-02 12:48:22 -0400

    move user input to start of the file

--- snpslicemain.R
+++ snpslicemain.R
@@ -12,29 +12,45 @@
 #
 ## import data -----
 rm(list = ls());
+
+
+################# 
+################# start of user determined values
+################# 
 ## input file name----
-prefix <- "inputdata/scenario2";
+prefix <- "scenario1";
+inputdataname <- paste("inputdata/", prefix, sep = "");
 ## model----
 # model <- 0 ## cat
 # model <- 1 ## "pois"
 # model <- 2 ## "bin";
 model <- 3 ## negbin
+## prior parameters
+rho <- 0.5;
+alpha <- 2.6;
+## mcmc parameters
+nmcmc <- 10000;
+gap <- 20; ## terminate the chain if lpost does not change in > gap steps
+burnin <- 0; ## this is the minimum number of MCMC iterations, default is 0.
+rep <- 1;
+## decide which information to store
+store_mcmc <- FALSE;
+store_sink <- FALSE;
+store_map <- TRUE;
+
+################# 
+################# end of user determined values
+################# 
+
+## read values from command lind input
+invisible(eval(parse(text=commandArgs(TRUE))));
+if(is.na(burnin)) burnin <- floor(nmcmc / 2); ## default for burnin
 ## threshold to determine single infections
 threshold <- 0.001;
 ## parameters for the observation model
 e1 <- 0.05;
 e2 <- 0.05;
-## prior parameters
-rho <- 0.5;
-alpha <- 2.6;
-## mcmc parameters
-nmcmc <- 20;
-gap <- 20; ## terminate the chain if lpost does not change in > gap steps
-burnin <- NA;
-rep <- 1;
-## read command line inputs
-invisible(eval(parse(text=commandArgs(TRUE))));
-if(is.na(burnin)) burnin <- floor(nmcmc / 2); ## default for burnin
+
 set.seed(rep);
 ## data processing-----
 model <- c("cat", "pois", "bin", "neg", "jpois")[model + 1];
@@ -56,14 +72,10 @@ if(model == "cat"){
   rho <- sum(y, na.rm = T) / sum(r, na.rm = T);
 }
 
-# inspect the prepared data
-# head(y);
-# r[1,1] <- NA;
-# y[1,1] <- NA;
 
-# helper functions for slice sampler-----
-source(paste("snpslice", model, ".R", sep = ""));
-source("snpslicecommon.R");
+# load helper functions for slice sampler-----
+source(paste("source/snpslice", model, ".R", sep = ""));
+source("source/snpslicecommon.R");
 
 
 cat("running", model, "model on data", prefix, "\n");
@@ -81,13 +93,13 @@ map <- list();
 lpostmax <- -Inf;
 mapiter <- 0;
 mapktrunc <- state$ktrunc;
-sink(file = paste("sink_", prefix,"_", model, "_nmcmc", nmcmc, "_gap", gap,"_rep", rep, ".txt", sep = ""), split = TRUE, type = "output");
+if(store_sink){
+  sink(file = paste("output/sink_", prefix,"_", model, "_nmcmc", nmcmc, "_gap", gap,"_rep", rep, ".txt", sep = ""), split = TRUE, type = "output");
+}
 for(iter in 1 : nmcmc){
 	state <- sliceIter(state);
-	# cat(iter,  sum(colSums(state$A) > 0), state$kstar, dim(state$A)[2],  sum(rowSums(state$A) ==1), state$mu[state$kstar], state$mu[state$ktrunc], state$logpost, lpostmax, "\n");
 	cat(iter,  sum(colSums(state$A) > 0), state$kstar, dim(state$A)[2],  sum(rowSums(state$A) ==1), state$logpost, lpostmax, "\n");
-	## if ktrunc changes, restart map
-	if(state$ktrunc > mapktrunc){
+	if(state$ktrunc > mapktrunc){## if ktrunc changes, restart map
 	  map <- state;
 	  mapiter <- iter;
 	  mapktrunc <- state$ktrunc;
@@ -99,16 +111,20 @@ for(iter in 1 : nmcmc){
   }
 	if(iter > burnin & mapiter < iter - gap) break;
 }
-sink();
+if(store_sink){
+  sink();
+}
 
 ## save the final sample (MAP estimator)
 A <- map$A[,which(colSums(map$A) > 0)];
-write.table(A, paste("output/output_", prefix, "_", model, "_A_nmcmc", nmcmc, "_gap", gap, "_rep", rep, ".txt", sep = ""),
+write.table(A, paste("output/", prefix, "_", model, "_A_nmcmc", nmcmc, "_gap", gap, "_rep", rep, ".txt", sep = ""),
             append = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE);
 D <- map$D[which(colSums(map$A) > 0),];
-write.table(D, paste("output/output_", prefix, "_", model, "_D_nmcmc", nmcmc, "_gap", gap, "_rep", rep, ".txt", sep = ""),
+write.table(D, paste("output/", prefix, "_", model, "_D_nmcmc", nmcmc, "_gap", gap, "_rep", rep, ".txt", sep = ""),
             append = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE);
 
 ## save MCMC state to RData file (for warm starts)
-filename <- paste("output/mcmc_", prefix, "_", model, "_nmcmc", nmcmc, "_gap", gap, "_rep", rep,  ".RData", sep = "");
-save(state, rho, alpha, e1, e2, model,r, y,  rep, prefix, file = filename);
+if(store_mcmc){
+  filename <- paste("output/mcmc_", prefix, "_", model, "_nmcmc", nmcmc, "_gap", gap, "_rep", rep,  ".RData", sep = "");
+  save(state, rho, alpha, e1, e2, model,r, y,  rep, prefix, file = filename);
+}
